33289566c6462b51dd580efff2ca9c62ef09c5f4
Mano-j <manojm.mailid@gmail.com>
2019-05-26 21:04:00 +0530

    Database change - ref: settins.py

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -77,9 +77,10 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'NAME' : 'hospital',
+        'USER' : 'hospadmin',
+        'PASSWORD' : 'h0Sp@sss',
+        'HOST' : 'localhost',        
     }
 }
 
